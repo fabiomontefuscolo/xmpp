01214498829248a926689349cc9502799521987a
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2025-09-02 11:05:48 +0200

    Set PHP 8.1 (closes #2 #3)
    * fix: remove old php version defs
    * fix: make setUp and tearDown compatible to PHPUnit\Framework\TestCase
    * fix: tests passing in php8.1

--- tests/src/Stream/SocketClientTest.php
+++ tests/src/Stream/SocketClientTest.php
@@ -70,7 +70,7 @@ class SocketClientTest extends TestCase
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->path    = sys_get_temp_dir() . '/phpunit_' . uniqid();
         //touch($this->path);
@@ -85,7 +85,7 @@ class SocketClientTest extends TestCase
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         unlink($this->path);
         fclose($this->server);
